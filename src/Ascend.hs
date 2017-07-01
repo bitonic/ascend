a039c5d4cb452004009bd9f5dfed176ead232a75
Francesco Mazzoli <f@mazzo.li>
2017-07-01 11:45:59 +0200

    comments

--- src/Ascend.hs
+++ src/Ascend.hs
@@ -9,15 +9,18 @@
 {-# LANGUAGE FlexibleInstances #-}
 module Ascend where
 
-import Data.Text (Text)
-import qualified Data.Text as T
 import Control.Monad (ap, unless, when)
 import Data.Traversable (for)
 import Data.Void (Void, absurd)
 import Data.String (IsString(fromString))
 
+-- a datatype to forget
+-- --------------------------------------------------------------------
+
 newtype Forget a = Forget {remember :: a}
   deriving (Functor, Foldable, Traversable, IsString)
+forget :: a -> Forget a
+forget = Forget
 
 instance Eq (Forget a) where
   _ == _ = True
@@ -32,27 +35,45 @@ instance Monad Forget where
   return = Forget
   Forget a >>= f = f a
 
-type Binder = Forget Text
+-- expressions
+-- --------------------------------------------------------------------
+
+type Binder = Forget String
 
 data Var a =
     F a
   | B Binder
   deriving (Functor, Foldable, Traversable, Eq, Ord, Show)
 
-data Phase = Earth | Heaven
+data Phase =
+    Earth -- "run time"
+  | Heaven -- "compile time"
   deriving (Eq, Ord, Show)
 
 type Type = Exp
 
+-- we store binders for easier pretty-printing,
+-- forgetting about them so that they won't
+-- matter for ==
 data Exp a =
     -- Types
     Type
-  | Pi Binder Phase (Type a) (Type (Var a))
+  | Pi
+      Binder
+      (Forget Phase)
+      -- we forget phases so that they won't matter for ==,
+      -- which we use for definitional equality (see
+      -- 'checkEqual')
+      (Type a)
+      (Type (Var a))
     -- Values
   | Lam Binder (Exp (Var a))
     -- Neutral
   | Neutral a [Exp a]
   deriving (Functor, Foldable, Traversable, Eq, Show)
+
+-- substitutions
+-- --------------------------------------------------------------------
 
 instance Applicative Exp where
   pure = return
@@ -62,6 +83,11 @@ instance Applicative Exp where
 ma >>>= mf = ma >>= (\case
   B b -> return (B b)
   F v -> fmap F (mf v))
+
+inst :: (Monad f) => f (Var a) -> f a -> f a
+inst fun arg = fun >>= (\case
+  B{} -> arg
+  F v -> return v)
 
 instance Monad Exp where
   return v = Neutral v []
@@ -76,11 +102,7 @@ app :: Exp a -> [Exp a] -> Exp a
 app f = \case
   [] -> f
   arg : args -> case f of
-    Lam _b body -> app
-      (body >>= (\case
-        B{} -> arg
-        F v -> return v))
-      args
+    Lam _b body -> app (inst body arg) args
     Neutral v args' -> Neutral v (args' ++ args)
     Type -> error "applying to a Type"
     Pi{} -> error "applying to a Pi"
@@ -94,15 +116,19 @@ extend env (phase, ty_) = \case
     (phase', ty') = env v
     in (phase', fmap F ty')
 
-type IsVar a = (Eq a)
+type IsVar a = (Eq a, Show a)
 
 checkEqual :: (IsVar a) => Exp a -> Exp a -> Either String ()
 checkEqual a b = do
+  -- very dumb and cheap def. equality
   unless (a == b) (Left "expressions not equal")
 
+-- check that the required phase is greater or
+-- equal than the actual phase. light subtyping
+-- if you wish
 checkPhase :: Phase -> Phase -> Either String ()
-checkPhase required given = do
-  unless (given <= required) $
+checkPhase required actual = do
+  unless (actual <= required) $
     Left ("Bad phase: " ++ show (required, given))
 
 check ::
@@ -110,24 +136,34 @@ check ::
   => Env a -> Phase -> Type a -> Exp a -> Either String ()
 check env phase ty = \case
   Type -> do
-    checkPhase phase Heaven
+    checkPhase phase Heaven -- types cannot be on earth
     checkEqual Type ty
   Pi _b domPhase dom cod -> do
-    checkPhase phase Heaven
+    checkPhase phase Heaven -- types cannot be on earth
     checkEqual Type ty
+    -- type check the domain and codomain type in heaven
     check env Heaven Type dom
-    check (extend env (domPhase, dom)) Heaven Type cod
+    check (extend env (remember domPhase, dom)) Heaven Type cod
   Lam _b body -> case ty of
+    -- for lambdas, just traverse them keeping the phase
     Pi _b domPhase dom cod -> do
-      check (extend env (domPhase, dom)) phase cod body
+      check (extend env (remember domPhase, dom)) phase cod body
     _ -> do
       Left "non-Pi type for Lam"
   Neutral v args -> do
     let (phase', funTy) = env v
+    -- check that the phase of the function is compatible
+    -- with the required phase. e.g. if the function is
+    -- irrelevant (in heaven), we'll fail to type check
+    -- it for a relevant phase (on earth)
     checkPhase phase phase'
+    -- then check the spine using the provided phase
+    -- (which might be higher than phase')
     ty' <- checkSpine env phase funTy args
+    -- check that the inferred type is equal
     checkEqual ty ty'
 
+-- takes the least upper bound of two phases
 lub :: Phase -> Phase -> Phase
 lub a b = case (a, b) of
   (Earth, Earth) -> Earth
@@ -142,7 +178,21 @@ checkSpine env phase ty = \case
   [] -> return ty
   arg : args -> case ty of
     Pi _b domPhase dom cod -> do
-      check env (lub phase domPhase) dom arg
+      -- check the argument:
+      --
+      --   function | argument | accepted
+      --   ---------+----------+---------
+      --   heaven   | heaven   | heaven
+      --   heaven   | earth    | heaven
+      --   earth    | heaven   | heaven
+      --   earth    | earth    | earth
+      --
+      -- the surprising one is on the second row.
+      -- intuitively, if the function as a whole
+      -- is irrelevant, it'll never be executed,
+      -- and thus we can discharge its arguments
+      -- too
+      check env (lub phase (remember domPhase)) dom arg
       checkSpine
         env
         phase
@@ -152,19 +202,23 @@ checkSpine env phase ty = \case
         args
     _ -> Left "non-Pi type for application"
 
+-- erasure
+-- --------------------------------------------------------------------
+
 data UExp a =
     ULam Binder (UExp (Var a))
   | UNeutral a [UExp a]
   deriving (Functor, Foldable, Traversable, Eq, Show)
 
+-- type directed
 erase :: Env a -> Type a -> Exp a -> Either String (UExp a)
 erase env ty = \case
   Type -> Left "Type in erasure"
   Pi{} -> Left "Pi in erasure"
   Lam b body0 -> case ty of
     Pi _b domPhase dom cod -> do
-      body <- erase (extend env (domPhase, dom)) cod body0
-      case domPhase of
+      body <- erase (extend env (remember domPhase, dom)) cod body0
+      case remember domPhase of
         Heaven -> do
           for body $ \case
             B{} -> Left "free var in erased body"
@@ -190,14 +244,14 @@ eraseSpine env ty = \case
           B{} -> arg0
           F v -> return v))
         args0
-      case domPhase of
+      case remember domPhase of
         Heaven -> return args
         Earth -> do
           arg <- erase env dom arg0
           return (arg : args)
     _ -> Left "non-Pi type in application and eraseSpine"
 
--- DSL
+-- small dsl to type expressions in haskell
 -- --------------------------------------------------------------------
 
 abstract :: (Functor f, Eq a) => a -> Binder -> f a -> f (Var a)
@@ -207,17 +261,17 @@ ety :: Exp a
 ety = Type
 
 (==>) :: (String, Phase, Type String) -> Type String -> Type String
-(v, phase, dom) ==> cod = Pi b phase dom (abstract v b cod)
+(v, phase, dom) ==> cod = Pi b (forget phase) dom (abstract v b cod)
   where
-    b = Forget (T.pack v)
+    b = forget v
 
 (-->) :: (Phase, Type String) -> Type String -> Type String
-(phase, dom) --> cod = Pi "" phase dom (fmap F cod)
+(phase, dom) --> cod = Pi "" (forget phase) dom (fmap F cod)
 
 elam :: String -> Exp String -> Exp String
 elam v body = Lam b (abstract v b body)
   where
-    b = Forget (T.pack v)
+    b = forget v
 
 eapp :: String -> [Exp String] -> Exp String
 eapp = Neutral
@@ -227,6 +281,9 @@ ev v = Neutral v []
 
 instance IsString (Exp String) where
   fromString = ev
+
+-- a few tests
+-- --------------------------------------------------------------------
 
 test :: String -> [(String, Phase, Type String)] -> Phase -> Type String -> Exp String -> IO ()
 test name vars00 phase ty0 e0 = do
@@ -255,7 +312,7 @@ test name vars00 phase ty0 e0 = do
         ty' <- traverse getv ty
         go
           (\v' -> if v == v'
-              then return (B (Forget (T.pack v)))
+              then return (B (forget v))
               else F <$> getv v')
           (extend env (tyPhase, ty'))
           vars
